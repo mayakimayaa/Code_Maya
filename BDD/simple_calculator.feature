8b23e9c5c9c4716d36af02484a42db708849032d
Maya Agarwal <maya.agarwal2@gmail.com>
2025-07-06 23:26:18 +0100

    Implemented it for Subtraction, Multiplication

--- BDD/simple_calculator.feature
+++ BDD/simple_calculator.feature
@@ -4,10 +4,38 @@ Scenario: Adding two integers
     Given I have an integer <first>
     And I have another integer <second>
     When I add the two integers
-    Then The result is <sum>
+    Then The result is <result>
 
 Examples:
-    | first | second | sum |
+    | first | second | result |
     | 3     | 5      | 8   |
     | 10    | 15     | 25  |
     | -2    | 4      | 2   |
+
+Scenario: Subtracting two integers
+    Given I have an integer <first>
+    And I have another integer <second>
+    When I subtract the two integers
+    Then The result is <result>
+    
+Examples:
+  | first | second | result |
+  | 3     | 5      | -2     |
+  | 10    | 15     | -5     |
+  | -2    | 4      | -6     |
+
+Scenario: Multiplying two integers
+    Given I have an integer <first>
+    And I have another integer <second>
+    When I multiply the two integers
+    Then The result is <result>
+    
+Examples:
+  | first | second | result |
+  | 3     | 5      | 15     |
+  | 10    | 15     | 150    |
+  | -2    | 4      | -8     |
+
+
+
+
--- BDD/test_simp_calculator.py
+++ BDD/test_simp_calculator.py
@@ -7,34 +7,58 @@ from pytest_bdd import (
     scenario,
     then,
     when,
+    parsers,
 )
 
 
-@fixture()
-def silly_calculator():
-    """Make a Silly Calculator"""
-    return SillyCalculator()
+@fixture
+def memory():
+    return {}
+
+# @fixture()
+# def silly_calculator():
+#     """Make a Silly Calculator"""
+#     return SillyCalculator()
 
 
-# @scenario('simple_calculator.feature', 'Adding two integers')
+@scenario('simple_calculator.feature', 'Adding two integers')
 def test_adding_two_integers():
     """Adding two integers."""
     pass
 
-@given('I have an integer {first:d}')
-def i_have_an_integer(context, first):
-    context.first = first
+@scenario('simple_calculator.feature', 'Subtracting two integers')
+def test_adding_two_integers():
+    """Subtracting two integers."""
+    pass
 
-@given('I have another integer {second:d}')
-def i_have_another_integer(context, second):
-    context.second = second
+@scenario('simple_calculator.feature', 'Multiplying two integers')
+def test_adding_two_integers():
+    """Multiplying two integers."""
+    pass
+
+@given(parsers.parse('I have an integer {first:d}'))
+def i_have_an_integer(memory, first):
+    memory['first'] = first
+
+@given(parsers.parse('I have another integer {second:d}'))
+def i_have_another_integer(memory, second):
+    memory['second'] = second
 
 @when("I add the two integers")
-def i_add_the_two_integers(context):
+def i_add_the_two_integers(memory):
     """I add the two integers."""
-    context.sum = context.first+context.second
+    memory['result'] = memory['first'] + memory['second']
 
+@when("I subtract the two integers")
+def i_subtract_the_two_integers(memory):
+    """I subtract the two integers."""
+    memory['result'] = memory['first'] - memory['second']
 
-@then('The result is {expected:d}')
-def the_result_is(context,expected):
-    assert context.sum == expected
+@when("I multiply the two integers")
+def i_multiply_the_two_integers(memory):
+    """I multiply the two integers."""
+    memory['result'] = memory['first'] * memory['second']
+
+@then(parsers.parse('The result is {expected:d}'))
+def the_result_is(memory,expected):
+    assert memory['result'] == expected
